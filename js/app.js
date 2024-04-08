ac8ad28a44915654471812a8b793e9aa5f203842
Lucas Mineiro <lucas0asm@outlook.com>
2024-04-07 23:53:03 -0300

    confirmação alugar/devolver

--- js/app.js
+++ js/app.js
@@ -5,12 +5,18 @@ function alterarStatus(id) {
     let nomeJogoSelecionado = gameSelecionado.querySelector('.dashboard__item__name');
     
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        // adicionar confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogoSelecionado.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
+        // adicionar confirmação antes de alugar o jogo
+        if (confirm(`Você tem certeza que deseja alugar o jogo ${nomeJogoSelecionado.textContent}?`)) {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+        }
     }
 }
