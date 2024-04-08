c1cedc969242c46e3b1323aee7195c75318f583e
Lucas Mineiro <lucas0asm@outlook.com>
2024-04-08 00:03:14 -0300

    Mostrar no console a quantidade de jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
+
 function alterarStatus(id) {
     let gameSelecionado = document.getElementById(`game-${id}`);
     let imagem = gameSelecionado.querySelector('.dashboard__item__img');
@@ -10,6 +21,7 @@ function alterarStatus(id) {
             imagem.classList.remove('dashboard__item__img--rented');
             botao.classList.remove('dashboard__item__button--return');
             botao.textContent = 'Alugar';
+            jogosAlugados--;
         }
     } else {
         // adicionar confirmação antes de alugar o jogo
@@ -17,6 +29,9 @@ function alterarStatus(id) {
             imagem.classList.add('dashboard__item__img--rented');
             botao.classList.add('dashboard__item__button--return');
             botao.textContent = 'Devolver';
+            jogosAlugados++;
         }
+
+        contarEExibirJogosAlugados();
     }
 }
