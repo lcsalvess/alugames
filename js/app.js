571b62e9636642fa7579860886daf7018af5cac1
Lucas Mineiro <lucas0asm@outlook.com>
2024-04-07 23:47:25 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id) {
+    let gameSelecionado = document.getElementById(`game-${id}`);
+    let imagem = gameSelecionado.querySelector('.dashboard__item__img');
+    let botao = gameSelecionado.querySelector('.dashboard__item__button');
+    let nomeJogoSelecionado = gameSelecionado.querySelector('.dashboard__item__name');
+    
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
